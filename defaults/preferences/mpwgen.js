4b87d1759ee5abe21b1054a8b17daafd9f6f4548
Anthony Lieuallen <arantius@gmail.com>
2006-12-23 16:55:52 +0000

    Closes #2
     * Add a keyboard shortcut.  `Ctrl-Alt-M` for English, `M` may be something else in other language builds.

--- defaults/preferences/mpwgen.js
+++ defaults/preferences/mpwgen.js
@@ -1,3 +1,4 @@
 pref("extensions.{afb4d196-e762-4d4c-a74d-d5ac21475f62}.description", "chrome://mpwgen/locale/overlay.properties");
 pref('mpwgen.username', '');
 pref('mpwgen.email', '');
+pref('mpwgen.autoCompOff', false);
